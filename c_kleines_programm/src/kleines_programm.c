16775a08a61a6fd3e9d0500ac493f2758786f635
CodingWithLaura <laura.ganss@web.de>
2021-10-26 23:20:55 +0200

    Add malloc in example function

--- c_kleines_programm/src/kleines_programm.c
+++ c_kleines_programm/src/kleines_programm.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-  printf("Hallo kleines Programm\n");
+  int ne_crazy_variable;
+  int* pointer = 0;
+
+  pointer = (int*)malloc(16*sizeof(int));
+  printf("Hallo kleines Programm variable: %d\n",ne_crazy_variable);
+
+  for(int i = 0;i<16;i++)
+  {
+    printf("i: %d wert: %d\n",i,pointer[i]);
+  }
+  free(pointer);
   return 0;
 }
